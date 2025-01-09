4df3870523b0d4bf55b7853dea726205437f8f41
butterfly <serenitaey39@gmail.com>
2025-01-09 18:11:52 +0900

    ブラウザに表示

---
+++ src/resources/views/auth/register.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/register.css') }}">
+@endsection
+
+@section('content')
+<div class="register">
+    <h1>Register</h1>
+</div>
+@endsection
